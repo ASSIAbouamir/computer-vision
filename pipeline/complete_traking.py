b0df18df69704918d57d8e5f6d4e1cb8c4dd8da7
ELAKIL Hakima <elakilhakima02@gmail.com>
2025-11-27 16:48:00 +0100

    Update complete_traking.py

--- pipeline/complete_traking.py
+++ pipeline/complete_traking.py
@@ -21,7 +21,7 @@ class PretrainedReIDModel:
             T.Normalize(mean=[0.485, 0.456, 0.406],
                          std=[0.229, 0.224, 0.225])
         ])
-        print("✅ Modèle Re-ID initialisé")
+        print(" Modèle Re-ID initialisé")
 
     def _load_model(self):
         model = models.resnet50(pretrained=True)
